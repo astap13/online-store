567af7176064526a07dd282405dd7ae9091657e4
shtag <vasyakanishchev@gmail.com>
2022-12-18 23:03:46 +0200

    refactor: main file

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,4 @@
 import './style.scss';
 import './normalize.css';
 import './components/product-details/product-details';
-
-function route(event: Event): any {
-    event = event || window.event;
-    event.preventDefault();
-    console.log(event);
-    window.history.pushState({}, '', (event.target as HTMLAnchorElement).href);
-    console.log(window.location.pathname);
-}
-
-async function handle(): Promise<void> {
-    const path = window.location.pathname;
-    const route = 1;
-}
-
-document.querySelectorAll('.link').forEach((el) => {
-    el.addEventListener('click', route);
-});
+import './components/router';
