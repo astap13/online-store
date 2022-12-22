a66319178f2244f7024db2a1c746260a257273a8
shtag <vasyakanishchev@gmail.com>
2022-12-23 01:34:59 +0200

    refactor:+historyApiFallback & CopyPlugin for HTML

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const baseConfig = {
     entry: path.resolve(__dirname, './src/main.ts'),
@@ -44,7 +45,18 @@ const baseConfig = {
             template: path.resolve(__dirname, './src/index.html'),
             filename: 'index.html',
         }),
-        new CleanWebpackPlugin(),
+        new CopyPlugin({
+            patterns: [
+                { 
+                    from: 'src/components/**/*.html' ,
+                    to: 'pages/[name].html',
+                    globOptions: {
+                        ignore: ['*.ts', '*.scss'],
+                    }
+            },
+        ],
+          }),
+        //new CleanWebpackPlugin(),
     ],
 };
 
