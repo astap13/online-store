b2416a432e8b334715345304413877b83b51df39
Kiryl Astapenka <81929875+astap13@users.noreply.github.com>
2022-12-26 21:21:24 +0100

    feat: add switching the type of products found

--- src/components/search/search.ts
+++ src/components/search/search.ts
@@ -4,6 +4,7 @@ class Search {
     static search() {
         throw new Error('Method not implemented.');
     }
+
     async renderSearch() {
         const root = document.querySelector('.products') as HTMLElement;
         const route = '/pages/search.html';
@@ -13,23 +14,57 @@ class Search {
         root.prepend(searchElement);
         const stat = PRODUCTS.length;
         document.querySelector('.stat')!.innerHTML = `Found: ${stat}`;
+        this.viewMode();
     }
-
-    //     search() {
-    //         (document.querySelector('.searhProducts') as HTMLInputElement).oninput = function () {
-    //             const val = this.value.trim();
-    //             const items = document.querySelectorAll('.products_item');
-    //             if (val != '') {
-    //                 items.forEach(function (elem) {
-    //                     if (elem.innerText.search(val) == -1){
-    //                         elem.classList.add('hide');
-    //                     } else {elem.classList.remove('hide');}
-
-    //                     }
-    //                 });
-    //             }
-
-    //         }
-    //     }
+    async viewMode() {
+        const smallBtn = document.querySelector('.small-v') as HTMLButtonElement;
+        const bigBtn = document.querySelector('.big-v') as HTMLButtonElement;
+        const itemInfo = document.querySelectorAll('div.item_info') as NodeListOf<Element>;
+        const item = document.querySelectorAll('li.products_item') as NodeListOf<Element>;
+        console.log(itemInfo);
+        const products = document.querySelectorAll('.products_item');
+        smallBtn.addEventListener('click', () => {
+            if (bigBtn.classList.contains('active-mode')) {
+                smallBtn.classList.toggle('active-mode');
+                bigBtn.classList.toggle('active-mode');
+                itemInfo.forEach((el) => {
+                    el.classList.toggle('hide');
+                });
+                item.forEach((el) => {
+                    el.classList.toggle('itemSmall');
+                });
+            }
+        });
+        bigBtn.addEventListener('click', () => {
+            if (smallBtn.classList.contains('active-mode')) {
+                smallBtn.classList.toggle('active-mode');
+                bigBtn.classList.toggle('active-mode');
+                itemInfo.forEach((el) => {
+                    el.classList.toggle('hide');
+                });
+                item.forEach((el) => {
+                    el.classList.toggle('itemSmall');
+                });
+            }
+        });
+    }
 }
+
+//     search() {
+//         (document.querySelector('.searhProducts') as HTMLInputElement).oninput = function () {
+//             const val = this.value.trim();
+//             const items = document.querySelectorAll('.products_item');
+//             if (val != '') {
+//                 items.forEach(function (elem) {
+//                     if (elem.innerText.search(val) == -1){
+//                         elem.classList.add('hide');
+//                     } else {elem.classList.remove('hide');}
+
+//                     }
+//                 });
+//             }
+
+//         }
+//     }
+
 export default Search;
