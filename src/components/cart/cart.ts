b58c4fb8b7e204e1d4a5e8aa34de22f71c85e513
shtag <vasyakanishchev@gmail.com>
2022-12-30 23:29:19 +0200

    feat: drop form cart

--- src/components/cart/cart.ts
+++ src/components/cart/cart.ts
@@ -1,9 +1,11 @@
 import './cart.scss';
 import { cartItems, IProductItem, cartItemType } from '../../types';
 import { capetalize, createElementWithClass } from '../../functions';
+import Checkout from './checkout';
 
 class Cart {
     cart: cartItems;
+    checkout: Checkout;
     _cartSum: number;
     _cartNum: number;
     constructor(cart: IProductItem) {
@@ -16,14 +18,17 @@ class Cart {
         this.cart.pop();
         this._cartSum = cart.price;
         this._cartNum = 1;
+        this.checkout = new Checkout();
     }
     async renderCart(): Promise<void> {
+        this.cleanCart();
         console.log('renderCart');
         const route = '/pages/cart.html';
         const html = await fetch(route).then((data) => data.text());
         const cartElement = document.createElement('div');
         cartElement.innerHTML = html;
         this.cart.forEach((product, id) => {
+            if (!product.amount) return;
             const cartItem = createElementWithClass('div', 'cart_item');
             const cartNumberItem = createElementWithClass('div', 'cart_item_number');
             const cartItemInfo = createElementWithClass('div', 'cart_item_info');
@@ -48,16 +53,23 @@ class Cart {
             cartProducts?.append(cartItem);
             const plusBtn = createElementWithClass('button', 'cart_plus_btn') as HTMLButtonElement;
             plusBtn.textContent = '+';
+            plusBtn.classList.add('btn_mini');
             const itemAmount = createElementWithClass('div', 'cart_item_amount_num');
             itemAmount.textContent = product.amount.toString();
             const minusBtn = createElementWithClass('button', 'cart_minus_btn') as HTMLButtonElement;
+            minusBtn.classList.add('btn_mini');
             minusBtn.textContent = '-';
             const cartControlles = cartAmount.childNodes[3] as HTMLElement;
             cartControlles?.append(minusBtn, itemAmount, plusBtn);
-            minusBtn.addEventListener('click', () => {
-                this.decreaseAmountItem(product);
+            minusBtn.addEventListener('click', (e) => {
                 itemAmount.textContent = this.cart[id].amount.toString();
+                this.decreaseAmountItem(product);
                 this.setSumNum();
+                if (!JSON.stringify(this.cart.filter((el) => el.amount > 0)).includes(product.description)) {
+                    const item = (e.target as HTMLElement).closest('.cart_item') as HTMLElement;
+                    item.classList.add('hide');
+                }
+                this.showCart();
             });
             plusBtn.addEventListener('click', () => {
                 this.increaseAmountItem(product);
@@ -72,6 +84,7 @@ class Cart {
         totalOfProducts.textContent = `Total: ${this.cartSum}`;
         const buyBtn = createElementWithClass('button', 'cart_buy_btn') as HTMLButtonElement;
         buyBtn.textContent = 'BUY NOW';
+        buyBtn.addEventListener('click', this.checkout.openPopup);
         sumBody.append(numOfProducts, totalOfProducts, buyBtn);
         this.setSumNum();
     }
@@ -99,7 +112,15 @@ class Cart {
             ...item,
             amount: 1,
         };
-        this.cart.push(productInCart);
+        if (!JSON.stringify(this.cart).includes(item.description)) {
+            console.log(item);
+            this.cart.push(productInCart);
+        } else if (this.cart.find((el) => el.amount === 0)) {
+            const elem = this.cart.find((el) => el.amount === 0);
+            if (elem) elem.amount++;
+        } else {
+            console.log('else');
+        }
         this.setSumNum();
     }
     increaseAmountItem(item: cartItemType): void {
@@ -109,15 +130,29 @@ class Cart {
                 el.amount++;
             }
         });
+        this.setSumNum();
     }
     decreaseAmountItem(item: cartItemType): void {
         console.log('decreaseAmountItem');
         item.amount--;
-        if (!item.amount) {
+        this.setSumNum();
+        /* if (!item.amount) {
             this.cart.forEach((el, n) => {
                 if (el.id === item.id) this.cart.splice(n, 1);
             });
+        } */
+    }
+    cleanCart() {
+        console.log(this.cart.filter((el) => el.amount > 0));
+        return this.cart.filter((el) => el.amount > 0);
+    }
+    drop(item: IProductItem) {
+        const ell = this.cart.find((el) => el.description === item.description);
+        if (ell) {
+            ell.amount = 0;
         }
+        this.cleanCart();
+        this.setSumNum();
     }
     showCart(): void {
         console.log(this.cart);
