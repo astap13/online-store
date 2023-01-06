f96eb18ee632b9636903b56f204b6915a7c73ba6
shtag <vasyakanishchev@gmail.com>
2023-01-06 21:53:59 +0200

    refactor: function createinput -> functions.ts

--- src/functions.ts
+++ src/functions.ts
@@ -6,5 +6,11 @@ function createElementWithClass(el: string, cla: string): HTMLElement {
     element.classList.add(cla);
     return element;
 }
+function createInput(classname: string, type?: string, placeholder?: string) {
+    const element = createElementWithClass('input', classname) as HTMLInputElement;
+    if (type) element.type = type;
+    if (placeholder) element.placeholder = placeholder;
+    return element;
+}
 
-export { createElementWithClass, capetalize };
+export { createElementWithClass, createInput, capetalize };
